d7eeeaf07b933387ed5afb2a59a9f55068c7f442
SamueleTerraroli <terraroli99@gmail.com>
2024-11-27 18:35:53 +0100

    add: objects array

---
+++ script.js
@@ -0,0 +1,22 @@
+const biciclette = [
+    {
+      name: 'Bici rossa',
+      peso: 33,
+    },
+    {
+      name: 'Bici nera',
+      peso: 25,
+    },
+    {
+      name: 'Bici viola',
+      peso: 20,
+    },
+    {
+      name: 'Bici verde',
+      peso: 17,
+    },
+    {
+      name: 'Bici bianca',
+      peso: 23,
+    },
+];
