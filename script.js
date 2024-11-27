ac45a024ed6cff0f5f7ebd8325f4c450b9cf8fe1
SamueleTerraroli <terraroli99@gmail.com>
2024-11-27 18:39:50 +0100

    feat: add for cicle

--- script.js
+++ script.js
@@ -22,3 +22,7 @@ const biciclette = [
 ];
 
 let biciclettaPiuLeggera = biciclette[0];
+for (let i = 1; i < biciclette.length; i++) {
+     if (biciclette[i].peso < biciclettaPiuLeggera.peso) {
+        biciclettaPiuLeggera = biciclette[i];
+    } }
